d1f6478e016d1a27a8b21e0be1597ab8af40030c
Oleksii <ilnytsky06@gmail.com>
2025-09-19 19:56:38 -0600

    changed page.js
    awesome sauce!

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Oleksii Ilnytsky</h2>
+      <p>
+        <Link href="https://github.com/Oleksii2006/cprg306-assignments">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
