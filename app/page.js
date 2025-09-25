ba247aba4abc1138785ab39ddaad8051ccbed209
Oleksii <ilnytsky06@gmail.com>
2025-09-25 08:32:08 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="http://localhost:3000/week-2">
+        <Link href="week-2">
           Week-2 Page
         </Link>
       </p>
