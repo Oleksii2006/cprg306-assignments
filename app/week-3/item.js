d3dd20e65bfddaff6b0154964667790879cd5269
Oleksii <ilnytsky06@gmail.com>
2025-09-25 21:54:47 -0600

    Yeah

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
  export default function Item({name, quantity,category})
 {
     return(
-        <div> 
+        <div className="border border-slate-800 bg-slate-800 w-full max-w-xs m-4 p-2 self-center"> 
             <ul>
                 <li>
-                    <h2>{name}</h2>
+                    <h2 className="text-xl font-bold">{name}</h2>
                     <p>buy {quantity} in {category}</p>
                 </li>
             </ul>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,8 +1,8 @@
 import DisplayItems from "./item-list";
 export default function Page() {
   return (
-    <main>
-      <h1>Shopping List</h1>
+    <main >
+      <h1 className="text-4xl font-bold m-6  flex flex-col items-center">Shopping List</h1>
       <DisplayItems/>
     </main>
   );
