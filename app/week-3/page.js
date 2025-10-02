633e8cdb4b88c6f5e8f591fdc59b6649e049d5a6
Oleksii <ilnytsky06@gmail.com>
2025-10-02 10:05:21 -0600

    problems solved

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ export default function Page() {
   return (
     <div>
       <title>week-3</title>
-      <h1 className="text-white text-4xl font-bold m-6  flex flex-col items-center">Shopping List</h1>
+      <h1 className="flex flex-col items-center text-white border-slate-800 bg-slate-800 w-full max-w-xs m-4 p-2 ">Shopping List</h1>
       <ItemList/>
     </div>
   );
