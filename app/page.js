7ee0b7fa43da08c606b67c639cc80f97e7aa71c7
Oleksii <ilnytsky06@gmail.com>
2025-10-02 08:09:17 -0600

    title

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
       <div>
+        <title>CPRG 306: Web Development 2 - Assignments</title>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         <Link href="week-2">
