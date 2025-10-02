3a9220cf42f4d7fda2f2e80aa7c7aa36574b74e4
Oleksii <ilnytsky06@gmail.com>
2025-10-02 09:52:59 -0600

    color

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
  export default function Item({name, quantity,category})
 {
     return(
-        <div className="border border-slate-800 bg-slate-800 w-full max-w-xs m-4 p-2 self-center"> 
+        <div className="text-white border-slate-800 bg-slate-800 w-full max-w-xs m-4 p-2 self-center "> 
             <ul>
                 <li>
                     <h2 className="text-xl font-bold">{name}</h2>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,10 @@
 import ItemList from "./item-list";
 export default function Page() {
   return (
-    <main >
+    <div>
       <title>week-3</title>
-      <h1 className="text-4xl font-bold m-6  flex flex-col items-center">Shopping List</h1>
+      <h1 className="text-white text-4xl font-bold m-6  flex flex-col items-center">Shopping List</h1>
       <ItemList/>
-    </main>
+    </div>
   );
 }
